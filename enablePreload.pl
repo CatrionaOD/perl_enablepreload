3f3d47cb58f9e0b64165dd7380711328ca41a2ef
CatrionaOD <odriscc4@tcd.ie>
2018-03-22 15:50:12 +0000

    Update enablePreload.pl
    Adding spaces between different functionalities

--- enablePreload.pl
+++ enablePreload.pl
@@ -7,10 +7,12 @@ use File::Find;
 use File::Copy qw(copy);
 use File::Basename;
 chdir "/home/e078272/2sys_apps_01/apache/server20cent/bin/startups";
+
 ### Stores all the instances
 my @search_files = <"C:/cygwin64/home/e078272/2sys_apps_01/apache/server20cent/bin/startups/apachectl.*">;
 ### Stores instances that have no preload enabled
 my @found_files;
+
 ### Searches for instances without preload and pushes them into found_files array
 foreach my $file (@search_files) {
   my $counter=0;
@@ -28,6 +30,7 @@ foreach my $file (@search_files) {
   close $file;
 }
 my @conf_files = <"C:/cygwin64/home/e078272/2sys_apps_01/apache/server20cent/conf/*.conf">;
+
 ### Checks through found_files array ANDing them with the conf files to create change_array with the instances to change.
 my @change_array;
 my @printchange_array;
@@ -49,6 +52,7 @@ if (scalar(@printchange_array)==0) {
   print "--No files found.";
 }
 print ("\n=======================================================");
+
 # Make backup of files in change_array of format filename_todaysdate.
 my($day,$mon,$year) = (localtime)[3,4,5];
 $mon = sprintf '%02d', $mon+1;
@@ -68,6 +72,7 @@ if (scalar(@bkup_files) == 0) {
   print "--No backup files created.";
 }
 print ("\n=======================================================");
+
 ### Add missing lines to the files
 if (scalar(@change_array) != 0) {
   print "\nMissing lines are being added to files.";
@@ -98,6 +103,7 @@ foreach my $file (@change_array) {
   rename ("$file.new", $file) || die "Unable to rename: $!";
 }
 print ("\n\n**Preload successfully enabled!**\n");
+
 ## For each file in change_array, we restart the instance. If it doesn't successfully restart in 30 seconds, user is notified and     the backup file is restarted instead.
 foreach my $file (@change_array) {
   my($filename, $directories, $suffix) = fileparse($file);
